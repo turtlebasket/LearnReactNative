46557bda26b7ff4781d378e76c1286b465cee876
turtlebasket <turtlebasket@gmail.com>
2021-01-07 09:05:06 -0800

    fix btn

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -18,14 +18,14 @@ class Home extends Component {
   }
 
   updateState = (text: string) => this.setState({testText: text})
-  updateStateCount = () => this.setState({testText: "Button Pressed", count: this.state.count+1})
-  resetCount = () => this.setState({count: 0})
+  updateStateCount = () => this.setState({testText: "Button Pressed "+(this.state.count+1), count: this.state.count+1})
+  resetCount = () => this.setState({count: 0, testText: "Count reset!"})
 
   render() {
     return (
       <View style = {styles.container}>
         <Text style={styles.pageTitle}>Epic Button Demo</Text>
-        <Text>{this.state.testText} {this.state.count}</Text>
+        <Text>{this.state.testText}</Text>
         <View style={styles.ctrlBtns}>        
           <View style={styles.ctrlBtn}><Button title="A Big Button!" onPress={this.updateStateCount} /></View>
           <View style={styles.ctrlBtn}><Button title="Reset Count" onPress={this.resetCount} /></View>
